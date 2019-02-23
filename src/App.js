6e90d79146b036fc222544c86a488e1a3e63ffa6
Stuart Jehn <stuartjehn@gmail.com>
2019-02-23 18:13:45 -0500

    component did mount sucessfully fetches api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      data: [],
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  async componentDidMount() {
+    // const {data} = await axios.get('https://api.staging.coord.co/codechallenge/commits')
+  }
+
   render() {
     return (
       <div className="App">
