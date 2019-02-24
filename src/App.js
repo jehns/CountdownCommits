225a8a68b393a82e15068c137a3ed503d1c7dda1
Stuart Jehn <stuartjehn@gmail.com>
2019-02-23 19:38:08 -0500

    component did mount now adds remaining time and data to state. setInterval updates state every second with new remaining time.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ class App extends Component {
     super()
     this.state={
       data: [],
-      hours: 0,
-      minutes: 0,
-      seconds: 0
+      timeLeft: 0
     }
   }
 
@@ -18,13 +16,43 @@ class App extends Component {
     // let xValues = new Array(data.length).map((num, i) => i)
 
 
+    // ************ test data **************
+    let xValues = [0,1,2,3,4]
+    let yValues = [3,7,11,39,44]
+
+    // create function for finding linear regression
+    const linearRegressionCalc = linearRegressionWrapper()
+
+    // calculate linear regression with api data
+    let linearRegressionResult = linearRegressionCalc(xValues, yValues)
+
+    console.log('linear reg', linearRegressionResult)
+
+    // Find the remaining time from current commit to the 2000th commit
+    let remainingTimeInSeconds = (linearRegressionResult.m*(2000) + linearRegressionResult.b) - (linearRegressionResult.m*(xValues.length) + linearRegressionResult.b)
+
+    // set state with fetched data and remaining time
+    this.setState({
+      data: [],
+      timeLeft: remainingTimeInSeconds
+    })
+
+    // call setInterval on window object to update the estimated remaining time on state
+    setInterval(() => {
+      this.setState({
+        data: [],
+        timeLeft: this.state.timeLeft - 1
+      })
+    }, 1000);
 
   }
 
   render() {
     return (
       <div className="App">
-
+        <ul>
+          <li>{this.state.timeLeft}</li>
+        </ul>
       </div>
     );
   }
