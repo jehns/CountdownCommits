335dd0104fc0b017b7b958c437ae8d83267d75e5
Stuart Jehn <stuartjehn@gmail.com>
2019-02-23 19:10:22 -0500

    linear regression function complete.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import linearRegressionWrapper from './scripts/linearRegression';
+
 
 class App extends Component {
   constructor() {
@@ -13,6 +15,10 @@ class App extends Component {
 
   async componentDidMount() {
     // const {data} = await axios.get('https://api.staging.coord.co/codechallenge/commits')
+    // let xValues = new Array(data.length).map((num, i) => i)
+
+
+
   }
 
   render() {
---
+++ src/scripts/linearRegression.js
@@ -0,0 +1,97 @@
+const linearRegressionWrapper = () => {
+
+  let previousValues = {
+    hasBeenCalled: false
+  }
+
+  return (xValues, yValues) => {
+
+    if (previousValues.hasBeenCalled) {
+      // init previous variables with new values
+      let x = xValues[0]
+      let y = yValues[0]
+      let sumX = previousValues.sumX + x;
+      let sumY = previousValues.sumY + y;
+      let sumXY = previousValues.sumXY + x*y;
+      let sumXX = previousValues.sumXX + x*x;
+      let count = previousValues.count + 1;
+
+      /*
+        Remember the values for next function call
+        */
+      previousValues.hasBeenCalled = true;
+      previousValues.sumX = sumX;
+      previousValues.sumY = sumY;
+      previousValues.sumXY = sumXY;
+      previousValues.sumXX = sumXX;
+      previousValues.count = count;
+
+      /*
+      * Calculate m and b for the formula:
+      * y = x * m + b
+      */
+      let m = (count*sumXY - sumX*sumY) / (count*sumXX - sumX*sumX);
+      let b = (sumY/count) - (m*sumX)/count;
+
+      return {m, b}
+
+    }
+
+    /*
+    If this is the first time the function has been called, initialize variables to zero
+    */
+    let sumX = 0;
+    let sumY = 0;
+    let sumXY = 0;
+    let sumXX = 0;
+    let count = 0;
+
+    /*
+    * We'll use those variables for faster read/write access.
+    */
+    let x = 0;
+    let y = 0;
+
+    if (xValues.length !== yValues.length) {
+      throw new Error('The parameters xValues and yValues need to have same size!');
+    }
+
+    /*
+    * Calculate the sum for each of the parts necessary.
+    */
+    for (let i = 0; i < xValues.length; i++) {
+      x = xValues[i];
+      y = yValues[i];
+      sumX += x;
+      sumY += y;
+      sumXX += x*x;
+      sumXY += x*y;
+      count++;
+    }
+
+    /*
+    Remember the values for next function call
+    */
+    previousValues.hasBeenCalled = true;
+    previousValues.sumX = sumX;
+    previousValues.sumY = sumY;
+    previousValues.sumXY = sumXY;
+    previousValues.sumXX = sumXX;
+    previousValues.count = count;
+
+    /*
+    * Calculate m and b for the formula:
+    * y = x * m + b
+    */
+    let m = (count*sumXY - sumX*sumY) / (count*sumXX - sumX*sumX);
+    let b = (sumY/count) - (m*sumX)/count;
+
+    return {m, b}
+
+  }
+
+}
+
+export default linearRegressionWrapper;
+
+
